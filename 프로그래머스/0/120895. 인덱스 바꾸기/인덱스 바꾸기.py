361d9302de66dd14204a63197483d6e2c15c1a52
sykim0 <114639376+sykim0@users.noreply.github.com>
2025-03-19 21:21:31 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,7 @@
+def solution(my_string, num1, num2):
+    
+    my_list = list(my_string)
+    my_list[num1], my_list[num2] = my_list[num2], my_list[num1]
+    
+    answer = ''.join(my_list)
+    return answer
