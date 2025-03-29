a4b4e6df7be28a5a2ff77cb112e50e5c44c1f84b
sykim0 <114639376+sykim0@users.noreply.github.com>
2025-03-29 15:47:43 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,9 +1,9 @@
--- 코드를 작성해주세요
-# 코드와 스킬코드가 join되면서 중복값 발생
 SELECT
-    DISTINCT(d.ID), d.EMAIL, d.FIRST_NAME, d.LAST_NAME
-FROM SKILLCODES s
-INNER JOIN DEVELOPERS d
-ON s.CODE & d.SKILL_CODE # 왜 굳이 이진수..?
-WHERE s.NAME IN ('C#', "Python")
-ORDER BY 1
+   DISTINCT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS d
+INNER JOIN SKILLCODES s
+ON d.SKILL_CODE & s.CODE
+WHERE 
+    s.NAME = 'Python' OR
+    s.NAME = 'C#' 
+ORDER BY d.ID ASC
