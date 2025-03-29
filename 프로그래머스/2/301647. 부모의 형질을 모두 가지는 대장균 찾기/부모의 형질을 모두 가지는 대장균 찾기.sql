15875b4dc07afb19535cc7eee122a3b72af20711
sykim0 <114639376+sykim0@users.noreply.github.com>
2025-03-29 15:56:10 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,16 +1,7 @@
-WITH child_and_parent AS(
-    SELECT
-        child.ID, child.GENOTYPE AS GENOTYPE,
-        parent.GENOTYPE AS PARENT_GENOTYPE
-    FROM ECOLI_DATA child
-    INNER JOIN ECOLI_DATA parent
-    ON child.PARENT_ID = parent.ID
-)
-
-# SELECT * FROM child_and_parent
-
 SELECT
-    * 
-FROM child_and_parent
-WHERE (GENOTYPE & PARENT_GENOTYPE) = PARENT_GENOTYPE
-ORDER BY 1 ASC
+    child.ID, child.GENOTYPE, parent.GENOTYPE AS PARENT_GENOTYPE
+FROM ECOLI_DATA child
+INNER JOIN ECOLI_DATA parent
+ON child.PARENT_ID = parent.ID
+WHERE parent.GENOTYPE = child.GENOTYPE & parent.GENOTYPE
+ORDER BY child.ID
