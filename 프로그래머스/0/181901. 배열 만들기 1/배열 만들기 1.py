31313e04d6daf82c676ae637f2cf09426c48fc2b
sykim0 <114639376+sykim0@users.noreply.github.com>
2025-04-24 19:08:13 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.22 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,2 +1,3 @@
 def solution(n, k):
-    return [num for num in range(1, n + 1) if num % k == 0]
+    # return [num for num in range(1, n + 1) if num % k == 0]
+    return [num for num in range(k, n + 1, k)]
